3e6989dec6b4ff641096345bb99034bd297d56bf
sandilyaprasoon9-hue <sandilyaprasoon9@gmail.com>
2026-02-12 00:28:32 +0530

    Update api_app.py

--- api_app.py
+++ api_app.py
@@ -1,24 +1,23 @@
 import streamlit as st
 from inference_sdk import InferenceHTTPClient
 from PIL import Image
-import tempfile
 
 st.title("Wall Infrastructure Detection")
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg","jpeg","png"])
+uploaded_file = st.file_uploader("Upload image", type=["jpg","png","jpeg"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    image.save(temp_file.name)
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image")
 
     CLIENT = InferenceHTTPClient(
         api_url="https://serverless.roboflow.com",
         api_key="YOUR_API_KEY"
     )
 
-    result = CLIENT.infer(temp_file.name, model_id="wall-infrastructure-detection/2")
+    result = CLIENT.infer(uploaded_file, model_id="wall-infrastructure-detection/2")
 
     st.write(result)
+
+
